032b8cbc5021b8ea7ccd18b0aaae5e1cfc479998
Mihai Cornel <mhcrnl@gmail.com>
2022-11-18 19:26:08 +0200

    Create 005_hello_world.py
    18.11.2022

---
+++ 005_hello_world.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# FILE: 005_hello_world.py
+# RUN : python3 005_hello_world.py
+# --------------------------------------------------------------Begin file
+print("Hello, World!")
+# ---------------------------------------------------------------End file
+
