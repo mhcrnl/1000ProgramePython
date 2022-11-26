9627883a050a3c532d0218cd1baa4167e23c021f
Mihai Cornel <mhcrnl@gmail.com>
2022-11-26 23:03:15 +0200

    26.11.2022(2)

--- 028_pytk_calculator_window/main.py
+++ 028_pytk_calculator_window/main.py
@@ -13,7 +13,7 @@ class Example(Frame):
 
     def initUI(self):
         self.master.title("Calculator window")
-        Style().configure("Button", padding=(0,5,0,5),font='serif 10')
+        Style().configure("TButton", padding=(0,5,0,5),font='serif 10')
 
         self.columnconfigure(0, pad=3)
         self.columnconfigure(1, pad=3)
@@ -29,11 +29,35 @@ class Example(Frame):
         entry = Entry(self)
         entry.grid(row=0, columnspan=4, sticky=W+E)
 
-        self.pack(fill=BOTH, expand=1)
+        cls = Button(self, text="Cls")
+        cls.grid(row=1, column=0)
+
+        bck = Button(self, text="Back")
+        bck.grid(row=1, column=1)
+
+        lbl = Button(self)
+        lbl.grid(row=1, column=2)
+        
+        clo = Button(self, text="Close", command=self.master.quit)
+        clo.grid(row=1, column=3)
+
+        seven = Button(self, text="7")
+        seven.grid(row=2, column=0)
+
+        eight = Button(self, text="8")
+        eight.grid(row=2, column=1)
+
+        nine = Button(self, text="9")
+        nine.grid(row=2, column=2)
+
+        div =  Button(self, text="/")
+        div.grid(row=2, column=3)
+
+        self.pack()
 
 def main():
     root = Tk()
-    root.geometry("250x150+300+300")
+    root.geometry("350x250+500+400")
     app = Example()
     root.mainloop()
 
